54fdf22c472bef294dbd71e58a50915c4e581356
starbox <starbox233@gmail.com>
2026-02-25 22:23:31 +0800

    Move GPS location fetching to handleConnect to bypass browser restrictions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { Toaster } from 'react-hot-toast';
@@ -7,34 +7,10 @@ import Dashboard from './components/Dashboard';
 import TacticalMap from './components/TacticalMap';
 import TeammateDetail from './components/TeammateDetail';
 import SOSModal from './components/SOSModal';
-import { useAppStore } from './store/useAppStore';
 
-export default function App() {
-  const { setCurrentGps } = useAppStore();
+export default function App()
+{
 
-  useEffect(() => {
-    // Request GPS permission and update every 10 seconds
-    if (navigator.geolocation) {
-      const updateGps = () => {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            setCurrentGps({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            });
-          },
-          (error) => {
-            console.error("GPS error:", error);
-          },
-          { enableHighAccuracy: true }
-        );
-      };
-      
-      updateGps();
-      const intervalId = setInterval(updateGps, 10000);
-      return () => clearInterval(intervalId);
-    }
-  }, [setCurrentGps]);
 
   return (
     <ThemeProvider theme={theme}>
